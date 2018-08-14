1c4003f552780f1b954a54a8f3c9684ba1ea4e95
angelicakr <angelicakarina.rivas@gmail.com>
2018-08-14 17:17:46 -0600

    second

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+
+$(document).ready(function () {
+
 var randomNum = 0;
 var crystal1 = 0;
 var crystal2 = 0;
@@ -9,11 +12,12 @@ var loses = 0;
 
 
 
+function allValues() {
 
 randomNum = Math.floor((Math.random() * 101)+ 19);
 console.log(randomNum);
 
-crystal1 = Math.floor((Math.random() * 11)+ 1);//random numbers for crystalmeth
+crystal1 = Math.floor((Math.random() * 11)+ 1);//random numbers for crystal
 console.log(crystal1);
 crystal2 = Math.floor((Math.random() * 11)+ 1);
 console.log(crystal2);
@@ -22,49 +26,65 @@ console.log(crystal3);
 crystal4 = Math.floor((Math.random() * 11)+ 1);
 console.log(crystal4);
 
-
-
-$(document).ready(function () {
+totalScore = 0;
 //to print random number
 $("#randomNum").text(randomNum);
-
+}
 
 //adding crystalmeth to the tweekers
 $("#crystal1").click(function(){
     totalScore += crystal1;
     $("#totalScore").text(totalScore); 
+    numberCheck()
 })
 $("#crystal2").click(function(){
     totalScore += crystal2;
     $("#totalScore").text(totalScore); 
+    numberCheck();
 })
 $("#crystal3").click(function(){
     totalScore += crystal3;
-    $("#totalScore").text(totalScore); 
+    $("#totalScore").text(totalScore);
+    numberCheck(); 
 })
 $("#crystal4").click(function(){
     totalScore += crystal4;
     $("#totalScore").text(totalScore); 
+    numberCheck();
 })
 console.log("randomNum" + randomNum);
 console.log("totalScore" + totalScore);
 //reset and win and lose
+allValues()
 
 function resetGame() {
-    $("#crystal1, #crystal2, #crystal3, #crystal4, #randomNum, #totalScore").empty();
+    console.log("working")
+    //$("#crystal1").text("");
+    //$("#crystal2").text("");
+    //$("#crystal3").text("");
+    //$("#crystal4").text("");
+    $("#randomNum").text("");
+    $("#totalScore").text("");
 
 }
 function numberCheck() {
-if ($("#totalScore") === randomNum) {
-    alert("You're a fucking winner bro!!");
+if (totalScore === randomNum) {
+    setTimeout(() => {
+        alert("You're a crystal winner");
+        resetGame();
+        allValues();
+    }, 500);
     wins++;
-    resetGame();
-    
+
 }
-if ($("#totalScore") > randomNum) {
-    alert("You're a crystal meth loser");
-    lose++;
-    resetGame();
+if (totalScore > randomNum) {
+    setTimeout(() => {
+        alert("You're a crystal loser");
+        resetGame();
+        allValues();
+    }, 500);
+    loses++;
+
 }
 }
 
